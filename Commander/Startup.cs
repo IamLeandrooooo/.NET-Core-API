d7f82dd976e89228db2d0c7fc4ab872a20fc08b4
IamLeandro <lyny_leandro@hotmail.com>
2020-07-04 22:29:03 +0100

    Added Swagger.UI to the API

--- Commander/Startup.cs
+++ Commander/Startup.cs
@@ -37,6 +37,8 @@ namespace Commander
             services.AddScoped<ICommanderRepo, SqlCommanderRepo>();
 
             services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+
+            services.AddSwaggerGen();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -46,6 +48,13 @@ namespace Commander
             {
                 app.UseDeveloperExceptionPage();
             }
+
+            app.UseSwagger();
+
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
+            });
 
             app.UseHttpsRedirection();
 
